9cdc2606d66bcc0bf5ce1f3c73987f861e59b584
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-05-05 22:08:31 +0530

    Dummy List

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React,{Fragment} from 'react';
 import Header from './Components/Header/Header';
 import Summary from './Components/Summary/Summary';
+import Meals from './Components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Summary />
+      <Meals />
     </Fragment>
   );
 }
---
+++ Components/Meals/Meals.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from './Meals.module.css'
+
+const DummyMeals = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest figh and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy....and green",
+    price: 18.99,
+  },
+];
+
+const Meals = () => {
+  return (
+    <section>
+      <Card>
+        <ul>
+          {DummyMeals.map((meal) => (
+        
+              <div className={classes.meals}>
+                <h3>{meal.name}</h3>
+                <div className={classes.desc}>{meal.description}</div>
+                <div className={classes.price}>{`$${meal.price.toFixed(2)}`}</div>
+              </div>
+              
+          ))}
+        </ul>
+      </Card>
+    </section>
+  );
+};
+
+export default Meals;
