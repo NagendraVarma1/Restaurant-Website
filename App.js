6e06741fe213e766dd93d4aeaa2702fa4bb63091
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-05-05 15:31:36 +0530

    Summary

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React,{Fragment} from 'react';
 import Header from './Components/Header/Header';
+import Summary from './Components/Summary/Summary';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Summary />
     </Fragment>
   );
 }
---
+++ Components/Summary/Summary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './Summary.module.css';
+
+const Summary = () => {
+    return <div className={classes.summary}>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>Choose your favourite meal from out broad selection of available meals and enhoy a delicious lunch or dinnter at home</p>
+        <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!</p>
+    </div>
+}
+
+export default Summary;
